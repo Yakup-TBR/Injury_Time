7b8cdc9dfb24fccf3f0fa7a60e35bb747c089736
rimuri03 <rizkimr@student.telkomuniversity.ac.id>
2024-11-04 14:04:28 +0700

    aaa

--- INKOSTEL/tests/Unit/ReviewTest.php
+++ INKOSTEL/tests/Unit/ReviewTest.php
@@ -1,99 +1,89 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
-use App\Models\Review;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Review;
+use App\Models\carikos;
 
 class ReviewTest extends TestCase
 {
     /** @test */
     public function it_can_add_a_review()
     {
-        // Data dummy untuk membuat review
+
         $data = [
             'id_kos' => '7',
             'user_id' => '1',
             'rating' => '5',
-            'comment' => 'Tempatnya bagus',
+            'comment' => 'Tempatnya bagus'
         ];
 
-        // Buat review menggunakan model
         Review::create($data);
 
-        // Periksa apakah data tersebut ada di tabel reviews
         $this->assertDatabaseHas('reviews', $data);
     }
 
     /** @test */
     public function it_requires_user_id_to_add_a_review()
     {
-        // Data dummy tanpa user_id
+
         $data = [
             'id_kos' => '7',
-            'user_id' => '1',
+            'user_id' => '1', //Fix
             'rating' => '5',
-            'comment' => 'Tempatnya bagus',
+            'comment' => 'Tempatnya bagus'
         ];
 
-        // Coba membuat review tanpa user_id
         Review::create($data);
 
-        // Periksa apakah data tersebut ada di tabel reviews
         $this->assertDatabaseHas('reviews', $data);
     }
 
     /** @test */
     public function it_requires_id_kos_to_add_a_review()
     {
-        // Data dummy tanpa id_kos
         $data = [
-            'id_kos' => '7',
+            'id_kos' => '7', //Fix
             'user_id' => '1',
             'rating' => '5',
-            'comment' => 'Tempatnya bagus',
+            'comment' => 'Tempatnya bagus'
         ];
 
-        // Coba membuat review tanpa id_kos
         Review::create($data);
 
-        // Periksa apakah data tersebut ada di tabel reviews
         $this->assertDatabaseHas('reviews', $data);
     }
 
     /** @test */
     public function it_requires_rating_to_add_a_review()
     {
-        // Data dummy tanpa rating
         $data = [
             'id_kos' => '7',
             'user_id' => '1',
-            'rating' => '5',
-            'comment' => 'Tempatnya bagus',
+            'rating' => '5', //Fix
+            'comment' => 'Tempatnya bagus'
         ];
 
-        // Coba membuat review tanpa rating
         Review::create($data);
 
-        // Periksa apakah data tersebut ada di tabel reviews
         $this->assertDatabaseHas('reviews', $data);
     }
 
     /** @test */
     public function it_can_add_a_review_with_empty_comment()
     {
-        // Data dummy dengan komentar kosong
         $data = [
             'id_kos' => '7',
             'user_id' => '1',
             'rating' => '4',
-            'comment' => '', // Komentar kosong
+            'comment' => '' // Komentar kosong
         ];
 
-        // Buat review dengan komentar kosong
         Review::create($data);
 
-        // Periksa apakah data tersebut ada di tabel reviews
         $this->assertDatabaseHas('reviews', $data);
     }
+
 }
