b4297e9854abd162c9f0e907a100ad6a7050e1e1
Tio Haidar <atiohaidar@gmail.com>
2024-11-04 14:24:35 +0700

    meningkatkan kualitas kode dengan mengubah index.blade.php

--- INKOSTEL/resources/views/saran/index.blade.php
+++ INKOSTEL/resources/views/saran/index.blade.php
@@ -8,7 +8,6 @@
   <title>Kotak Saran</title>
   <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@2.10.2/dist/umd/popper.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
   <link rel="stylesheet" href="../css/jualKos.css">
